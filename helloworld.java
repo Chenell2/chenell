76bbd0e6b2a345fef823c9bbabf677f5268b6bec
Chenell2 <chenellmitchell11@gmail.com>
2025-10-07 21:32:31 +0000

    firstjavalesson

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,13 @@
+public class helloworld { 
+    public static void main(String args[]){
+        System.out.println("helloworld");
+        System.out.println("dream big");
+        int bar=22;
+        int mars=11;
+        System.out.println(b*2);
+    
 
+    
+
+    } 
+ } 
